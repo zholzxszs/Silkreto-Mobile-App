1492a0d5388a98f98b22154f3fad49b9e479c9b3
Judiel Legaspina <legaspinajudiel10@gmail.com>
2026-01-11 20:42:36 +0800

    lacking: modern transition

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-// Replace with your actual home screen
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -9,29 +8,22 @@ class SplashScreen extends StatefulWidget {
   State<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>
-    with SingleTickerProviderStateMixin {
-  late AnimationController _animationController;
-  late Animation<double> _fadeAnimation;
+class _SplashScreenState extends State<SplashScreen> {
+  double _opacity = 0.0;
 
   @override
   void initState() {
     super.initState();
-    _setupAnimation();
+    _startAnimation();
     _navigateToNextScreen();
   }
 
-  void _setupAnimation() {
-    _animationController = AnimationController(
-      duration: const Duration(milliseconds: 800),
-      vsync: this,
-    );
-
-    _fadeAnimation = Tween<double>(begin: 0, end: 1).animate(
-      CurvedAnimation(parent: _animationController, curve: Curves.easeIn),
-    );
-
-    _animationController.forward();
+  void _startAnimation() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _opacity = 1.0;
+      });
+    });
   }
 
   Future<void> _navigateToNextScreen() async {
@@ -44,75 +36,86 @@ class _SplashScreenState extends State<SplashScreen>
 
     if (mounted) {
       if (hasSeenOnboarding) {
-        // User has seen onboarding, go to home screen
         Navigator.of(context).pushReplacementNamed('/home');
       } else {
-        // New user, show get started screen
         Navigator.of(context).pushReplacementNamed('/getStarted');
       }
     }
   }
 
   @override
-  void dispose() {
-    _animationController.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
+    final containerWidth = screenSize.width > 380 ? 380.0 : screenSize.width;
+    final containerHeight = containerWidth * (560 / 380);
+
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: FadeTransition(
-          opacity: _fadeAnimation,
-          child: Container(
-            width: 380,
-            height: 560,
-            clipBehavior: Clip.antiAlias,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: const Alignment(0.50, -0.00),
-                end: const Alignment(0.50, 1.00),
-                colors: [const Color(0xFF63A361), const Color(0xFF253D24)],
-              ),
+      backgroundColor: const Color(0xFF63A361),
+      body: AnimatedOpacity(
+        opacity: _opacity,
+        duration: const Duration(milliseconds: 800),
+        curve: Curves.easeIn,
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: const Alignment(0.50, -0.00),
+              end: const Alignment(0.50, 1.00),
+              colors: const [
+                Color(0xFF63A361), // #63A361
+                Color(0xFF253D24), // #253D24
+              ],
             ),
-            child: Stack(
-              children: [
-                Positioned(
-                  left: 98,
-                  top: 319,
-                  child: Text(
-                    'SILKRETO',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 32,
-                      fontFamily: 'Nunito',
-                      fontWeight: FontWeight.w900,
-                      letterSpacing: 3.20,
-                    ),
-                  ),
-                ),
-                Positioned(
-                  left: 122,
-                  top: 198,
-                  child: Container(
-                    width: 135,
-                    height: 135,
-                    decoration: BoxDecoration(
-                      image: DecorationImage(
-                        image: NetworkImage("https://placehold.co/135x135"),
-                        fit: BoxFit.cover,
+          ),
+          child: Center(
+            child: Container(
+              width: containerWidth,
+              height: containerHeight,
+              child: Stack(
+                children: [
+                  // Logo Image
+                  Positioned(
+                    left: containerWidth * (122 / 380),
+                    top: containerHeight * (230 / 560),
+                    child: Container(
+                      width: containerWidth * (135 / 380),
+                      height: containerWidth * (135 / 380),
+                      child: Image.asset(
+                        'assets/Silkreto-Logo.png',
+                        fit: BoxFit.contain,
                       ),
                     ),
                   ),
-                ),
-              ],
+
+                  // SILKRETO Text
+                  Positioned(
+                    left: 0,
+                    right: 0,
+                    top: containerHeight * (380 / 560),
+                    child: Text(
+                      'SILKRETO',
+                      textAlign: TextAlign.center,
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: containerWidth * (32 / 380),
+                        fontFamily: 'Nunito',
+                        fontWeight: FontWeight.w900,
+                        letterSpacing: containerWidth * (3.20 / 380),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
   }
 }
