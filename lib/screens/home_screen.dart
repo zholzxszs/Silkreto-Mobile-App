ddcb4baae06cb84a5c3e6620615657578fd66326
Judiel Legaspina <legaspinajudiel10@gmail.com>
2026-01-13 01:16:35 +0800

    remove the unncesseary settings and home nav

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -6,6 +6,8 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const HomeSection();
+    return const Scaffold(
+      body: HomeSection(),
+    );
   }
 }
